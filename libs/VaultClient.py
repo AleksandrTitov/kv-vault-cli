d2b39d2cde7bfd1c69a4a190a54ff2e46218bf80
Aleksandr Titov <titov.aleksandr@edbox.ru>
2019-03-04 18:03:36 +0300

    Add docs for VaultClient class

--- libs/VaultClient.py
+++ libs/VaultClient.py
@@ -6,6 +6,8 @@ Support the following action:
     - Get data
     - Write data
     - Delete data
+    - List of mounted secrets
+    - Enable KV secret engine
 """
 
 import urllib3
@@ -14,7 +16,9 @@ from libs.setup_logger import logger
 
 
 class VaultClient(object):
-    """Hashicorp Vault Client"""
+    """
+    Hashicorp Vault Client, uses API https://www.vaultproject.io/api/
+    """
 
     def __init__(self, vault_addr='http://127.0.0.1:8200', vault_token=None, api_ver='v1'):
         self.vault_addr = vault_addr
@@ -23,6 +27,14 @@ class VaultClient(object):
         self.log = logger
 
     def _http_request(self, method, path, data=None):
+        """
+        A common method for HTTP requests
+
+        :param method: HTTP method
+        :param path: URL path
+        :param data: data of the request
+        :return: HTTPResponse object
+        """
 
         url = f'{self.vault_addr}/{self.api_ver}/{path}'
         body = json.dumps(data)
@@ -40,6 +52,15 @@ class VaultClient(object):
         return request
 
     def get_list_of_secrets(self, path):
+        """
+        Get list data from Vault at the given path
+
+        https://www.vaultproject.io/api/secret/kv/kv-v1.html#list-secrets
+
+        :param path: URL path
+        :return: list data from Vault at the given path, as an example:
+            ['secret/', 'new']
+        """
 
         request = self._http_request('LIST', path)
 
@@ -53,7 +74,15 @@ class VaultClient(object):
         return list_of_secrets
 
     def get_data(self, path):
+        """
+        Get the value of the path
 
+        https://www.vaultproject.io/api/secret/kv/kv-v1.html#read-secret
+
+        :param path: URL path
+        :return: dictionary of the secrets, as an example:
+            {'secret1': '1', 'secret2': '2'}
+        """
         request = self._http_request('GET', path)
 
         if request.status != 200:
@@ -66,6 +95,14 @@ class VaultClient(object):
         return data
 
     def write_data(self, path, data):
+        """
+        Sets or updates data in the KV store
+
+        https://www.vaultproject.io/api/secret/kv/kv-v1.html#create-update-secret
+
+        :param path: URL path
+        :param data: Value in the following format {'new': '1'}
+        """
 
         request = self._http_request('GET', path)
 
@@ -79,6 +116,13 @@ class VaultClient(object):
             self.log.warning(f'Alarm! Something goes wrong...')
 
     def delete_data(self, path):
+        """
+        Delete operation on secrets in Vault
+
+        https://www.vaultproject.io/api/secret/kv/kv-v1.html#delete-secret
+
+        :param path: URL path
+        """
 
         request = self._http_request('DELETE', path)
 
@@ -88,6 +132,11 @@ class VaultClient(object):
             self.log.warning(f'Alarm! Something goes wrong...')
 
     def list_mounted_secrets_engines(self):
+        """
+        Get secrets engines
+
+        :return: https://www.vaultproject.io/api/system/mounts.html#sample-response
+        """
 
         path = 'sys/mounts'
         request = self._http_request('GET', path)
@@ -97,6 +146,14 @@ class VaultClient(object):
         return data
 
     def enable_secrets_engine(self, name, type="kv"):
+        """
+        Enable secrets engine
+
+        https://www.vaultproject.io/api/system/mounts.html#enable-secrets-engine
+
+        :param name: name of the secrets engine
+        :param type: type of the secrets engine
+        """
 
         path = f'sys/mounts/{name}'
 
